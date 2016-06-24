30cd0d4adc102176934a9360f038411ebc6f010d
Daria Dementieva <ddementieva@codenvy.com>
2016-06-24 15:28:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# web-go-simple
+
+Go Web Sample Application
+
+# Developer Workspace
+[![Contribute](http://beta.codenvy.com/factory/resources/codenvy-contribute.svg)](http://beta.codenvy.com)
+
+# Stack to use
+
+FROM [codenvy/ubuntu_go](https://hub.docker.com/r/codenvy/ubuntu_go/)
+
+# How to run
+
+| #       | Description           | Command  |
+| :------------- |:-------------| :-----|
+| 1      | Run | `cd ${current.project.path} && go get -d && go run main.go` |
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "log"
+    "net/http"
+)
+
+var (
+    message string
+    address string
+)
+
+func init() {
+    flag.StringVar(&message, "message", "there", "a message to print")
+    flag.StringVar(&address, "address", "0.0.0.0:8080", "address/port to listen on")
+}
+
+func main() {
+    flag.Parse()
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s\n", message)
+    })
+    log.Printf("listening on %s...", address)
+    log.Fatal(http.ListenAndServe(address, nil))
+}
